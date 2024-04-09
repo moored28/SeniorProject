c931e5b72303f9fa9c9a0bb400f0d866fa9e059d
Graeff78 <Vex831@gmail.com>
2024-04-09 13:06:23 -0400

    Task page overhaul

--- basic/urls.py
+++ basic/urls.py
@@ -6,5 +6,5 @@ app_name="basic"
 urlpatterns = [
     path("compute/<int:value>", views.compute, name="compute"),
     path('homepage/', views.homepage, name="homepage"),
-    path('assignments/', views.assignments, name="assignments")
+    path("assignments/<task_id>/", views.assignments, name="assignments")
 ]
